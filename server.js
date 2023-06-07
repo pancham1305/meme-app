792a5cacf3fa28bd09295a7058780c2f51c72c76
Pancham Mehta <pancham1305@gmail.com>
2023-06-07 15:03:51 +0530

    download fixed

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import path from "path";
+import fs, { createReadStream } from "fs";
 const app = express();
 const PORT = 3000;
 app.set("view engine", "ejs");
@@ -9,16 +10,39 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 const url = "https://meme-api.com/gimme";
 const data = await fetch(url).then((res) => res.json());
-app.get("/", (req, res) => {
-  res.render("index", { data });
+app.get("/", async (req, res) => {
+  let ImgUrl = data.url;
+  const response = await fetch(ImgUrl);
+  // const u = URL.createObjectURL(response);
+  // console.log(response);
+  res.render("index", { data, res: response });
 });
 
 app.post("/", async (req, res) => {
-  const { next } = req.body;
+  // const { next } = req.body;
   const url = "https://meme-api.com/gimme";
   const data = await fetch(url).then((res) => res.json());
   res.render("index", { data });
 });
+
+app.post("/download", async (req, res) => {
+  const { url } = req.body;
+  // console.log(url);
+  const response = await fetch(url, {
+    method: "GET",
+    headers: {
+      "Content-Type": "image/png",
+    },
+  });
+  res.setHeader("Content-Type", "image/png");
+  const arr = await response.arrayBuffer();
+  const buffer = Buffer.from(arr);
+
+  fs.createWriteStream("./public/meme.png").write(buffer);
+  res.setHeader("Content-Disposition", "attachment; filename=meme.png");
+  res.download("./public/meme.png");
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
